0cf85df1e862b8b9a1fabf87a8463c84b603cef2
btp2102 <65583877+btp2102@users.noreply.github.com>
2026-09-19 21:13:38 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Paste the /exec URL of your deployed Apps Script web app here.
 // It looks like: https://script.google.com/macros/s/AKfycb.../exec
 // This URL is not a secret or a credential; it is fine for it to be public.
-window.FAMILY_UPDATES_BACKEND_URL = 'PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE';
+window.FAMILY_UPDATES_BACKEND_URL = 'https://script.google.com/macros/s/AKfycbxYelOQABAl-0GJXt0eOhw3U0Tb92NqVIvLGrA97CTkuB6xY8qaCSU50qswn4qCevuJCw/exec';

3e4599d93315a70eea2b536d7d76869dd14555bc
btp2102 <65583877+btp2102@users.noreply.github.com>
2026-09-19 21:12:38 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Paste the /exec URL of your deployed Apps Script web app here.
+// It looks like: https://script.google.com/macros/s/AKfycb.../exec
+// This URL is not a secret or a credential; it is fine for it to be public.
+window.FAMILY_UPDATES_BACKEND_URL = 'PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE';
